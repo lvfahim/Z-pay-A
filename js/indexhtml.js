3b679e7ef3910825ba63f04597018fa56b4fc53f
lvfahim <mdfahim26320@gmail.com>
2025-08-22 23:38:15 +0600

    login from is complete

---
+++ js/indexhtml.js
@@ -0,0 +1,19 @@
+document.getElementById('login-btn').addEventListener('click',
+    function (){
+        const number= 1315345166;
+        const pin=2632;
+
+        const modileNumber =document.getElementById('mobile-input').value 
+        const modileNumberValue =parseInt(modileNumber)
+
+        const pinNumber =document.getElementById('pin-input').value 
+        const pinNumberValue =parseInt(pinNumber)
+
+        // console.log(modileNumberValue,pinNumberValue)
+        if(modileNumberValue=== number && pinNumberValue=== pin){
+           window.location.href="./home.html"
+        }else{
+            alert('Invalid Credentials')
+        }
+    }
+)
